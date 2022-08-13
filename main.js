be9c59ccf48ec73b008a5a9f1fa4a07009ec3676
chandni855 <108581984+chandni855@users.noreply.github.com>
2022-08-13 19:24:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_kill=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+    video=createCapture(VIDEO);
+	video.size(800,400);
+    video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelloaded);
+	poseNet.on('pose',gotposes);
+}
+
+function modelloaded()
+{
+	console.log('modal loaded');
+}
+
+function gotposes(result)
+{
+	if(result.lenght > 0)
+	{
+		console.log(result);
+		noseX=result[0].pose.nose.x;
+        noseY=result[0].pose.nose.y;
+	}
+}
+
+function draw() 
+{
+	game();
+
+}
+
+
+
+
+
+
